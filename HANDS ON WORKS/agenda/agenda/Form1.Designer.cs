a2efd694a3ed71476ae10f9b578179a1e81e60ff
willianlrl <56350957+willianlrl@users.noreply.github.com>
2020-09-21 18:45:02 -0300

    Add files via upload

---
+++ HANDS ON WORKS/agenda/agenda/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace agenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_cadastrar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btn_consultar = new System.Windows.Forms.Button();
+            this.btn_excluir = new System.Windows.Forms.Button();
+            this.gridTabela = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTabela)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(536, 38);
+            this.txtTelefone.Mask = "(00)00000-0000";
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefone.TabIndex = 1;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(178, 38);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(284, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(26, 38);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 8;
+            this.txtId.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "NOME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(533, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "TELEFONE";
+            // 
+            // btn_cadastrar
+            // 
+            this.btn_cadastrar.Location = new System.Drawing.Point(26, 97);
+            this.btn_cadastrar.Name = "btn_cadastrar";
+            this.btn_cadastrar.Size = new System.Drawing.Size(100, 23);
+            this.btn_cadastrar.TabIndex = 2;
+            this.btn_cadastrar.Text = "CADASTRAR";
+            this.btn_cadastrar.UseVisualStyleBackColor = true;
+            this.btn_cadastrar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(362, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "EDITAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_consultar
+            // 
+            this.btn_consultar.Location = new System.Drawing.Point(178, 97);
+            this.btn_consultar.Name = "btn_consultar";
+            this.btn_consultar.Size = new System.Drawing.Size(100, 23);
+            this.btn_consultar.TabIndex = 3;
+            this.btn_consultar.Text = "CONSULTAR";
+            this.btn_consultar.UseVisualStyleBackColor = true;
+            this.btn_consultar.Click += new System.EventHandler(this.btn_consultar_Click);
+            // 
+            // btn_excluir
+            // 
+            this.btn_excluir.Location = new System.Drawing.Point(536, 97);
+            this.btn_excluir.Name = "btn_excluir";
+            this.btn_excluir.Size = new System.Drawing.Size(100, 23);
+            this.btn_excluir.TabIndex = 5;
+            this.btn_excluir.Text = "EXCLUIR";
+            this.btn_excluir.UseVisualStyleBackColor = true;
+            this.btn_excluir.Click += new System.EventHandler(this.btn_excluir_Click);
+            // 
+            // gridTabela
+            // 
+            this.gridTabela.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridTabela.Location = new System.Drawing.Point(26, 150);
+            this.gridTabela.Name = "gridTabela";
+            this.gridTabela.Size = new System.Drawing.Size(610, 279);
+            this.gridTabela.TabIndex = 7;
+            this.gridTabela.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 450);
+            this.Controls.Add(this.gridTabela);
+            this.Controls.Add(this.btn_excluir);
+            this.Controls.Add(this.btn_consultar);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_cadastrar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtTelefone);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridTabela)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_cadastrar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_consultar;
+        private System.Windows.Forms.Button btn_excluir;
+        private System.Windows.Forms.DataGridView gridTabela;
+    }
+}
+
---
+++ HANDS ON WORKS/agenda/agenda/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;//adicionado para utilizar a referência Mysql.Data
+
+namespace agenda
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conexao;
+        SqlCommand comando;
+        SqlDataAdapter dataAdapter;
+        SqlDataReader dataReader;
+        string stringSql;
+        //Essas 5 linhas acima são as variáveis para facilitar durante o processo de utilização do SQL.
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\agenda\tb_agenda.mdf;Integrated Security=True"); //conexao recebe a string de conexao ao banco de dados
+                stringSql = "INSERT INTO TB_AGENDA(NOME, TELEFONE) VALUES (@NOME, @TELEFONE)";  //STRING SQL PARA INSERIR O REGISTRO NO BANCO DE DADOS
+                comando = new SqlCommand(stringSql, conexao);
+                comando.Parameters.AddWithValue("@NOME", txtNome.Text); //adiciona o valor digitado no campo txtnome para a variavel @nome
+                comando.Parameters.AddWithValue("@TELEFONE", txtTelefone.Text);//adiciona o valor digitado no campo txttelefone para a variavel @telefone
+
+                conexao.Open();  //abre a conexao com o banco de dados
+
+                comando.ExecuteNonQuery(); //O método ExecuteNonQuery é utilizado para executar instruções SQL que não retornam dados, como Insert, Update, Delete, e Set.
+            }
+
+            catch (Exception excecao)
+            {
+                MessageBox.Show(excecao.Message); //exibir a mensagem de exceção
+
+            }
+
+            finally
+            {
+                txtId.Text = "";
+                txtNome.Text = "";
+                txtTelefone.Text = "";
+                conexao.Close(); //fecha a conexão com o banco de dados 
+                conexao = null;
+                comando = null;
+            }
+            atualizaGrid();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\agenda\tb_agenda.mdf;Integrated Security=True"); //conexao recebe a string de conexao ao banco de dados
+                stringSql = "UPDATE TB_AGENDA SET NOME = @NOME, TELEFONE = @TELEFONE WHERE ID = @ID";  //STRING SQL PARA ATUALIZAR O REGISTRO NO BANCO DE DADOS QUANDO O ID DIGITADO É IGUAL A ALGUM ID NA BASE DE DADOS
+                comando = new SqlCommand(stringSql, conexao);
+                comando.Parameters.AddWithValue("@ID", txtId.Text);//adiciona o valor digitado no campo txtId para a variavel @ID
+                comando.Parameters.AddWithValue("@NOME", txtNome.Text); //adiciona o valor digitado no campo txtnome para a variavel @nome
+                comando.Parameters.AddWithValue("@TELEFONE", txtTelefone.Text);//adiciona o valor digitado no campo txttelefone para a variavel @telefone
+
+                conexao.Open();  //abre a conexao com o banco de dados
+
+                comando.ExecuteNonQuery(); //O método ExecuteNonQuery é utilizado para executar instruções SQL que não retornam dados, como Insert, Update, Delete, e Set.
+            }
+
+            catch (Exception excecao)
+            {
+                MessageBox.Show(excecao.Message); //exibir a mensagem de exceção
+
+            }
+
+            finally
+            {
+                txtId.Text = "";
+                txtNome.Text = "";
+                txtTelefone.Text = "";
+                conexao.Close(); //fecha a conexão com o banco de dados 
+                conexao = null;
+                comando = null;
+            }
+            atualizaGrid();
+        }
+
+        private void btn_excluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\agenda\tb_agenda.mdf;Integrated Security=True"); //conexao recebe a string de conexao ao banco de dados
+                stringSql = "DELETE FROM TB_AGENDA WHERE ID = @ID";  //STRING SQL PARA EXCLUIR O REGISTRO NO BANCO DE DADOS QUANDO O ID DIGITADO É IGUAL A ALGUM ID NA BASE DE DADOS
+                comando = new SqlCommand(stringSql, conexao);
+                comando.Parameters.AddWithValue("@ID", txtId.Text);//adiciona o valor digitado no campo txtId para a variavel @ID
+
+                conexao.Open();  //abre a conexao com o banco de dados
+
+                comando.ExecuteNonQuery(); //O método ExecuteNonQuery é utilizado para executar instruções SQL que não retornam dados, como Insert, Update, Delete, e Set.
+            }
+
+            catch (Exception excecao)
+            {
+                MessageBox.Show(excecao.Message); //exibir a mensagem de exceção
+
+            }
+
+            finally
+            {
+                txtId.Text = "";
+                txtNome.Text = "";
+                txtTelefone.Text = "";
+                conexao.Close(); //fecha a conexão com o banco de dados 
+                conexao = null;
+                comando = null;
+            }
+            atualizaGrid();
+        }
+
+        private void btn_consultar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\agenda\tb_agenda.mdf;Integrated Security=True"); //conexao recebe a string de conexao ao banco de dados
+                stringSql = "SELECT * FROM TB_AGENDA WHERE NOME = @NOME";  //STRING SQL PARA EXCLUIR O REGISTRO NO BANCO DE DADOS QUANDO O ID DIGITADO É IGUAL A ALGUM ID NA BASE DE DADOS
+                comando = new SqlCommand(stringSql, conexao);
+                comando.Parameters.AddWithValue("@NOME", txtNome.Text);//adiciona o valor digitado no campo txtId para a variavel @ID
+
+                conexao.Open();  //abre a conexao com o banco de dados
+
+                dataReader = comando.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    txtId.Text = Convert.ToString(dataReader["id"]); //busca o id no banco de dados e atualiza o nome no textbox txtId
+                    txtNome.Text = Convert.ToString(dataReader["nome"]); //busca o nome no banco de dados e atualiza o nome no textbox txtNome
+                    txtTelefone.Text = Convert.ToString(dataReader["telefone"]);//busca o telefone no banco de dados e atualiza o telefone no textbox txtTelefone
+                }
+            }
+
+            catch (Exception excecao)
+            {
+                MessageBox.Show(excecao.Message); //exibir a mensagem de exceção
+
+            }
+
+            finally
+            {
+                conexao.Close(); //fecha a conexão com o banco de dados 
+                conexao = null;
+                comando = null;
+            }
+            atualizaGrid();
+        }
+
+        private void atualizaGrid()
+        {
+            try
+            {
+                conexao = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\agenda\tb_agenda.mdf;Integrated Security=True"); //conexao recebe a string de conexao ao banco de dados
+                stringSql = "SELECT * FROM TB_AGENDA";  //STRING SQL PARA EXCLUIR O REGISTRO NO BANCO DE DADOS QUANDO O ID DIGITADO É IGUAL A ALGUM ID NA BASE DE DADOS
+
+                dataAdapter = new SqlDataAdapter(stringSql, conexao);
+                DataTable tabela = new DataTable(); //gera uma nova instancia do DataTable para exibir a tabela
+                dataAdapter.Fill(tabela); //popula a tabela
+
+                gridTabela.DataSource = tabela; //exibe a tabela
+                gridTabela.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill; //redimensiona a tabela para se ajustar ao tamanho do objeto pai
+
+
+            }
+
+            catch (Exception excecao)
+            {
+                MessageBox.Show(excecao.Message); //exibir a mensagem de exceção
+
+            }
+
+            finally
+            {
+                conexao.Close(); //fecha a conexão com o banco de dados 
+                conexao = null;
+                comando = null;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            atualizaGrid();
+        }
+    }
+}
